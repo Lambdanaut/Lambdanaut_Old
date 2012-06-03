144ea75f97636ebc690d75d0f3a145a6167276b7
Josh Thomas <personettelabs@gmail.com>
2012-06-02 22:32:50 -0400

    Updates an email link

--- src/Lambdanaut/views/welcome.clj
+++ src/Lambdanaut/views/welcome.clj
@@ -379,7 +379,7 @@
 	(common/layout "Contact"
 		[:h1 "Drop me a line"]
 		[:p 
-			"You can send me an " (link-to "mailto:personettelabs@gmail.com" "email") " or find me online at " (link-to "http://drawrawr.com/lambdanaut" "Drawrawr.com") ". I'm also on " (link-to "http://facebook.com/lambdanaut" "Facebook") ". "
+			"You can send me an " (link-to "mailto:josh@lambdanaut.com" "email") " or find me online at " (link-to "http://drawrawr.com/lambdanaut" "Drawrawr.com") ". I'm also on " (link-to "http://facebook.com/lambdanaut" "Facebook") ". "
 		]
 	)
 )
