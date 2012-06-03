50495ee85c33fa04f5c78cf1e58526d04c443b0c
Josh Thomas <personettelabs@gmail.com>
2012-06-03 00:17:51 -0400

    Small change in the meta description

--- src/Lambdanaut/views/common.clj
+++ src/Lambdanaut/views/common.clj
@@ -9,7 +9,7 @@
 	(html5
 		[:head
 			[:title "Lambdanaut :: " title]
-			[:meta {:name "description" :content "The personal home page of Joshua Thomas AKA Lambdanaut"} ]
+			[:meta {:name "description" :content "The personal home page of Josh Thomas AKA Lambdanaut"} ]
 			[:meta {:name "author" :content "Lambdanaut"} ]
 			[:meta {:name "keywords" :content "Lambdanaut Art Programming Haskell Clojure Python Internet Blog Joshua Thomas Josh DarkScythe Jelix Tealon Personette Lambda Functional Binary Robot Drawing Pixel Nunchuks"} ]
 			(include-css "/css/reset.css")
