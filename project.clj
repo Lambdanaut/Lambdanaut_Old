7a5115d14c465bdd41e0b534b921dc99d1d5b4f2
Josh Thomas <personettelabs@gmail.com>
2012-07-08 13:13:32 -0400

    Adds shadow to the navigation box.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject Lambdanaut "0.1.0-SNAPSHOT"
+(defproject Lambdanaut "1.0.0-SNAPSHOT"
             :description "The software that runs my personal site, lambdanaut.com"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]]
